fb02bb35e95a72510ea03be319b957b386a206a9
joaotanan <joaogtanan@gmail.com>
2026-03-14 09:19:20 -0300

    atualizado

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto');
-
-let numeroSecreto = 5;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 
 let chute;
@@ -9,10 +9,10 @@ let tentativas = 1;
 // enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
 
-    chute = Number(prompt('Escolha um número entre 1 e 10'));
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -22,3 +22,14 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
+//if (tentativas > 1) {
+//alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+//} else {
+//alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+//}
+// toda essa parte é a mesma coisa das linhas 
